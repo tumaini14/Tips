6a07e542ed542a32265646cae9fdbd70d7e0a662
tumaini14 <totamo14@gmail.com>
2017-01-05 01:34:25 -0600

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  tips
 //
-//  Created by Steve Buza on 12/4/15.
-//  Copyright © 2015 codepath. All rights reserved.
+//  Created by Steve Buza on 12/29/16.
+//  Copyright © 2016 codepath. All rights reserved.
 //
 
 import UIKit
