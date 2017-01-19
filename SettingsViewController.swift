740ad4c63ab9e0cbd0c48ddc9678ad3fcb5cb6f6
tumaini14 <totamo14@gmail.com>
2017-01-18 18:50:29 -0600

    Add files via upload

---
+++ SettingsViewController.swift
@@ -0,0 +1,128 @@
+﻿//
+//  SettingsViewController.swift
+//  tips
+//
+//  Created by Steve Buza on 12/4/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var darkLightControl: UISegmentedControl!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var splitBillField: UITextField!
+    @IBOutlet weak var customTipField: UITextField!
+    
+    //sets the background colors
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let colorDefaults = NSUserDefaults.standardUserDefaults()
+        let backgroundColor = colorDefaults.integerForKey("defaultTheme")
+        if backgroundColor == 0 {
+            view.backgroundColor = UIColor.darkGrayColor()
+            darkLightControl.selectedSegmentIndex = backgroundColor
+        }
+        else{
+            self.view.backgroundColor = UIColor(red: 57.0/255.0, green: 164.0/255.0, blue: 1.0/255.0, alpha: 1)
+            darkLightControl.selectedSegmentIndex = backgroundColor             
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //makes sure the tipControl is always on the last selected segment even if the user switches views
+        let tipDefault = NSUserDefaults.standardUserDefaults()
+        tipControl.selectedSegmentIndex = tipDefault.integerForKey("mySelectedSegment")
+        
+        //load the customTip default to reflect previous changes made
+        let customTipDefault = NSUserDefaults.standardUserDefaults()
+        customTipField.text = "\(customTipDefault.integerForKey("myCustomTip"))"
+        
+        //loads the splitBill default to reflect previous changes made
+        let splitBillDefault = NSUserDefaults.standardUserDefaults()
+        splitBillField.text = "\(splitBillDefault.integerForKey("mySplitBill"))"
+        
+        // Do any additional setup after loading the view.
+    
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // D23ispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onTipControlChange(sender: UISegmentedControl) {
+        
+        //selected segment array form
+        var tipPercentages = [0.10, 0.20, 0.25]
+        //tipPercentage is the selected segment
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        //saves the index value to later load
+        let selectedSegment = tipControl.selectedSegmentIndex
+        let tipControlDefault = NSUserDefaults.standardUserDefaults()
+        tipControlDefault.setInteger(selectedSegment, forKey:"mySelectedSegment")
+        //saves the tip
+        tipControlDefault.setDouble(tipPercentage, forKey:"myDefaultTip")
+        tipControlDefault.synchronize()
+    }
+    
+
+    @IBAction func onCustomTipChange(sender: AnyObject) {
+        //when customTip is changed 
+        //it saves the integer value to use in the calculating view
+        let customTip = NSString(string: customTipField.text!).integerValue
+        let customTipDefault = NSUserDefaults.standardUserDefaults()
+        customTipDefault.setInteger(customTip, forKey: "myCustomTip")
+        customTipDefault.synchronize()
+        customTipField.text = "\(customTip)"
+    }
+
+    @IBAction func onSplitBillChange(sender: AnyObject) {
+        //when splitBill is changed it saves the value to be used in the calculating view
+        let splitBill = NSString(string: splitBillField.text!).integerValue
+        let splitBillDefault = NSUserDefaults.standardUserDefaults()
+        splitBillDefault.setInteger(splitBill, forKey:"mySplitBill")
+        splitBillDefault.synchronize()
+        splitBillField.text = "\(splitBill)"
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+
+    //sets background color
+    @IBAction func updateDefaultTheme(sender: AnyObject) {
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        defaults.setInteger(darkLightControl.selectedSegmentIndex, forKey: "defaultTheme")
+        defaults.synchronize()
+        
+        if darkLightControl.selectedSegmentIndex == 0 {
+            self.view.backgroundColor = UIColor.darkGrayColor()
+        }
+        else {
+            self.view.backgroundColor = UIColor(red: 57.0/255.0, green: 164.0/255.0, blue: 1.0/255.0, alpha: 1)
+        }
+        
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TipViewController.swift
@@ -0,0 +1,215 @@
+﻿//
+//  TipViewController.swift
+//  tips
+//
+//  Created by Steve Buza on 12/4/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class TipViewController: UIViewController {
+    
+    
+    @IBOutlet var defaultViewColor: UIView!
+    @IBOutlet weak var splitLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipPercentLabel: UILabel!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let colorDefaults = NSUserDefaults.standardUserDefaults()
+        let backgroundColor = colorDefaults.integerForKey("defaultTheme")
+        if backgroundColor == 0 {
+            view.backgroundColor = UIColor.darkGrayColor()
+        }
+        else{
+            self.view.backgroundColor = UIColor(red: 57.0/255.0, green: 164.0/255.0, blue: 1.0/255.0, alpha: 1)
+        }
+        //new format. converts the string into a doubleValue
+        //loads the defaults and saves the customTip and splitBill numbers
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let tipControlDefault = NSUserDefaults.standardUserDefaults()
+        let tipPercentage = tipControlDefault.doubleForKey("myDefaultTip")
+        let customTipDefault = NSUserDefaults.standardUserDefaults()
+        let customTip = Double(customTipDefault.integerForKey("myCustomTip"))
+        let splitBillDefault = NSUserDefaults.standardUserDefaults()
+        let splitBill = Double(splitBillDefault.integerForKey("mySplitBill"))
+        
+        //displays if the user changed the custom tip to anything but zero
+        if customTip != 0.0
+        {
+            //displays if the user has split bill to zero
+            if splitBill == 0{
+                let tip = billAmount * (customTip/100)
+                let total = billAmount + tip
+                tipLabel.text = String(format: "$%.2f", tip)
+                totalLabel.text = String(format: "$%.2f", total)
+                splitLabel.text = "$0.00"
+                tipPercentLabel.text = "(\(Int(customTip))%)"
+                splitNumberLabel.text = "(\(Int(splitBill)))"
+            }
+            //displays if the user did change the split bill to anything but zero
+            else {
+                let tip = billAmount * (customTip/100)
+                let total = billAmount + tip
+                let split = (billAmount + tip)/splitBill
+                
+                tipLabel.text = String(format: "$%.2f", tip)
+                if billField.text == ""{
+                    splitLabel.text = "$0.00"
+                }
+                else{
+                    splitLabel.text = String(format: "$%.2f", split)
+                }
+                totalLabel.text = String(format: "$%.2f", total)
+                tipPercentLabel.text = "(\(Int(customTip))%)"
+                splitNumberLabel.text = "(\(Int(splitBill)))"
+            }
+        }
+        //displays if the user did not change the custom tip to anything but zero
+        else{
+            //displays if the user has the split bill to zero
+            if splitBill == 0{
+                let tip = billAmount * tipPercentage
+                let total = billAmount + tip
+                tipLabel.text = String(format: "$%.2f", tip)
+                totalLabel.text = String(format: "$%.2f", total)
+                splitLabel.text = "$0.00"
+                tipPercentLabel.text = "(\(Int(tipPercentage*100))%)"
+                splitNumberLabel.text = "(\(Int(splitBill)))"
+            }
+            //displays if the user did change the split bill to anything but zero
+            else{
+                let tip = billAmount * tipPercentage
+                let total = billAmount+tip
+                let split = (billAmount+tip)/splitBill
+                
+                tipLabel.text = String(format: "$%.2f", tip)
+                if billField.text == ""{
+                    splitLabel.text = "$0.00"
+                }
+                else{
+                    splitLabel.text = String(format: "$%.2f", split)
+                }
+                totalLabel.text = String(format: "$%.2f", total)
+                tipPercentLabel.text = "(\(Int(tipPercentage*100))%)"
+                splitNumberLabel.text = "(\(Int(splitBill)))"
+            }
+        }
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //sets bill field as first responder
+        billField.becomeFirstResponder()
+        
+        //sets the background color
+        let colorDefaults = NSUserDefaults.standardUserDefaults()
+        let backgroundColor = colorDefaults.integerForKey("myColor")
+        if backgroundColor == 0{
+            view.backgroundColor = UIColor.darkGrayColor()
+        }
+        else{
+            view.backgroundColor = defaultViewColor.backgroundColor
+        }
+        
+        //when the code is loaded for the first time this is displayed
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        splitLabel.text = "$0.00"
+        
+        //this is for when the user changes options in the settings view
+        //it will display the chosen custom tip or split number
+
+        let splitBillDefault = NSUserDefaults.standardUserDefaults()
+        let splitNumber = splitBillDefault.doubleForKey("mySplitBill")
+        splitNumberLabel.text = "(\(Int(splitNumber)))"
+        
+        let customTipDefault = NSUserDefaults.standardUserDefaults()
+        let customTip = Double(customTipDefault.integerForKey("myCustomTip"))
+        if customTip == 0{
+            let tipControlDefault = NSUserDefaults.standardUserDefaults()
+            let tipPercentage = tipControlDefault.doubleForKey("myDefaultTip")
+            tipPercentLabel.text = "(\(Int(tipPercentage)*100)%)"
+        }
+        else{
+            tipPercentLabel.text = "(\(Int(customTip))%)"
+        }
+        
+    
+        //this portion of code is to configure the navigation bars and the background color and fix the title colors
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage.init(), forBarMetrics: UIBarMetrics.Default)
+        self.navigationController?.navigationBar.translucent = true
+        self.navigationController?.view.backgroundColor = UIColor.clearColor()
+        self.navigationController?.navigationBar.backgroundColor = UIColor.clearColor()
+        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+        self.navigationController!.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
+    
+        
+        
+    }
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {        
+        
+        //loads the defaults to get information for calculations
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let tipControlDefault = NSUserDefaults.standardUserDefaults()
+        let tipPercentage = tipControlDefault.doubleForKey("myDefaultTip")
+        let customTipDefault = NSUserDefaults.standardUserDefaults()
+        let splitBillDefault = NSUserDefaults.standardUserDefaults()
+        let splitBill = Double(splitBillDefault.integerForKey("mySplitBill"))
+        let customTip = Double(customTipDefault.integerForKey("myCustomTip"))
+        
+        //calculates the tip, split, and total
+        if customTip != 0.0
+        {
+            if splitBill == 0{
+                let tip = billAmount * (customTip/100)
+                let total = billAmount + tip
+                tipLabel.text = String(format: "$%.2f", tip)
+                totalLabel.text = String(format: "$%.2f", total)
+                splitLabel.text = "$0.00"
+            }
+            else {
+                let tip = billAmount * (customTip/100)
+                let total = billAmount + tip
+                let split = (billAmount + tip)/splitBill
+                
+                tipLabel.text = String(format: "$%.2f", tip)
+                splitLabel.text = String(format: "$%.2f", split)
+                totalLabel.text = String(format: "$%.2f", total)
+            
+            }
+        }
+        else{
+            if splitBill == 0{
+                let tip = billAmount * tipPercentage
+                let total = billAmount + tip
+                tipLabel.text = String(format: "$%.2f", tip)
+                totalLabel.text = String(format: "$%.2f", total)
+                splitLabel.text = "$0.00"
+            }
+            else{
+                let tip = billAmount * tipPercentage
+                let total = billAmount+tip
+                let split = (billAmount+tip)/splitBill
+                
+                tipLabel.text = String(format: "$%.2f", tip)
+                splitLabel.text = String(format: "$%.2f", split)
+                totalLabel.text = String(format: "$%.2f", total)
+            }
+        }
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+}
